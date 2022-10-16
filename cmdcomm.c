aa7e974731981f9b574b64601334006bc079a363
Aditdev <adityalawate2004@gmail.com>
2022-10-16 09:39:37 +0530

    new commit

--- cmdcomm.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int a =1;
-    system("cls");
-    system("echo {a}");
-    return 0;
-}
--- commiter.py
+++ commiter.py
@@ -1,13 +1,13 @@
 import os
 initializer ="git init"
 add ="git add ."
-msg = input("COmmit message?\n") 
+msg = str(input("COmmit message?\n"))
 cmt = f"git commit -m {msg}"
 os.system(f"{initializer}")
 os.system(f"{add}")
 os.system(f"{cmt}")
 repolink = str(input("What link?\n"))
-os.system(f"git remote add newo {repolink}")
+os.system("git remote add newo {}".format(repolink))
 yN = input("want to push?")
 if yN =='Y':
     os.system("git push -u newo main")
