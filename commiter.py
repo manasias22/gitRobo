d30ffec3199fbf09ddcc138ca9d9aaaf1a85763a
Aditya Lawate codez <aditya.211343101@vcet.edu.in>
2022-10-16 09:21:00 +0530

    vs

--- commiter.py
+++ commiter.py
@@ -1,11 +1,12 @@
-
-# from multiprocessing.connection import wait
 import os
 initializer ="git init"
 add ="git add ."
 msg = input("COmmit message?\n") 
 cmt = f"git commit -m {msg}"
 os.system(f"{initializer}")
-os.system(f"{add}") 
-# wait(10)
+os.system(f"{add}")
+# os.system(f"git commit -m {msg}") 
 os.system(f"{cmt}")
+repolink = str(input("What link?\n"))
+os.system(f"git remote add origin {repolink}")
+os.system("git push origin")
