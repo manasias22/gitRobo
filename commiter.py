99a47c3ba32df1976f9c7e2dfb14d07b618eecc6
Aditdev <adityalawate2004@gmail.com>
2022-10-18 18:11:26 +0530

    last commit

--- commiter.py
+++ commiter.py
@@ -4,17 +4,24 @@
 # and input commit message and repo link. And that's it.
 # Make changes and then just run this file.
 
-import time
-import glob
 import os
+import time
+
+# Make a list of files in your directory
 dirlis = os.listdir()
 
+# to check if .git folder exists
 if ".git" not in dirlis:
     initializer = "git init"
     os.system(f"{initializer}")
 
+
 k = 1
-gitdirlis = os.listdir(".git\\refs\\remotes\\")
+
+#to check if we have newo added in gits directory.
+gitdirlis = os.listdir(".git\\refs\\remotes\\") 
+
+# Infinite loop for infinite commits to git.
 while True:
     msg = input("Commit message?\n")
     print("What would you like to add?\n 1 -> All file?\n 2 -> Recent modified file?")
@@ -25,7 +32,7 @@ while True:
         os.system(f"git add .")
 
     os.system(f"git commit -m \"{msg}\"")
-    yN = input("want to push (Y/N)?")
+    yN =input("want to push (Y/N)?")
     yN.capitalize();
     if yN =='Y':
         if k==1:
