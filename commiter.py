f781aeb30b596f37aa28247a837296952c5f0fc6
Aditdev <adityalawate2004@gmail.com>
2022-10-18 08:44:22 +0530

    no.

--- commiter.py
+++ commiter.py
@@ -13,8 +13,8 @@ os.system(f"{initializer}")
 os.system(f"{add}")
 os.system(f"{cmt}")
 yN = input("want to push?")
-
-if yN =='Y' or yN =='y':
+yN = yN.capitalize();
+if yN =='Y' or yN =='YES':
     repolink = str(input("What link?\n"))
     os.system("git remote add newo {}".format(repolink))
     os.system("git push -f newo main")
